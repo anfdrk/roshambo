0256b2388a6299fdd9e6b2496089d0c754752419
Andrei Fedaruk <anfdrk@gmail.com>
2023-01-20 22:15:27 +0300

    Сreate a "game" function that starts the game up to 5 wins

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
-'use strict'
+
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+game();
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   switch(randomNumber) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
   }
+}
+
+function getPlayerChoice () {
+  const choise = prompt('Choose your weapon');
+  return choise.toLowerCase();
+}
+
+function playRound (playerSelection, computerSelection) {
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+  if (playerSelection === computerSelection) {
+    console.log(`It's a tie!`)
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    scorePlayer++
+    console.log('You won! Rock beats scissors')
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    scorePlayer++
+    console.log('You won! Paper beats rock')
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    scorePlayer++
+    console.log('You won! Scissors beats paper')
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    scoreComputer++
+    console.log('You lost! Rock beats scissors')
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    scoreComputer++
+    console.log('You lost! Scissors beats paper')
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    scoreComputer++
+    console.log('You lost! Paper beats rock')
+  }
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
+  console.log(finalMessage());
+}
+
+function finalMessage() {
+  const winner = (scorePlayer > scoreComputer) ? 'Computer' : 'You';
+  return `${winner} win the game`
 }
